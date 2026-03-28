ac49e2479230c916f31fcd3796ca973c115b9d89
BARRETTMEAG <10504850@uvu.edu>
2026-03-27 19:13:29 -0600

    Rename genomes_annotated to bakta_genomes_annotated

--- scripts/BAKTA/run_bakta_filtered.sh
+++ scripts/BAKTA/run_bakta_filtered.sh
@@ -3,8 +3,8 @@ PROJECT=${PROJECT:-/path/to/project}
 
 DB="$PROJECT/db-light"
 GENOME_DIR="$PROJECT/genomes_raw"
-OUTPUT_DIR="$PROJECT/genomes_annotated"
-APPROVED="$PROJECT/scripts/approved_genomes.txt"
+OUTPUT_DIR="$PROJECT/bakta_genomes_annotated"
+APPROVED="$PROJECT/scripts/BUSCO/approved_genomes.txt"
 
 THREADS=6
 
--- scripts/BAKTA/summarize_bakta.sh
+++ scripts/BAKTA/summarize_bakta.sh
@@ -2,7 +2,7 @@
 
 PROJECT=${PROJECT:-/path/to/project}
 
-ANNOTATION_DIR="$PROJECT/genomes_annotated"
+ANNOTATION_DIR="$PROJECT/bakta_genomes_annotated"
 OUTPUT="$ANNOTATION_DIR/genome_summary.tsv"
 
 echo -e "Genome\tLength\tGC\tCodingDensity\tCDS\tHypotheticals\tPercentHypothetical\ttRNA\trRNA\tCRISPR" > "$OUTPUT"
